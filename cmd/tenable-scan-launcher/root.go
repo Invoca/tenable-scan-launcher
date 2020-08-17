7f2d3d848b8172e608c923731de64a0d69630ba0
maximillianbrain1 <mbrain@invoca.com>
2020-08-17 11:23:27 -0400

    Removed initCommand function

--- cmd/tenable-scan-launcher/root.go
+++ cmd/tenable-scan-launcher/root.go
@@ -121,37 +121,6 @@ instances given based on the scanner id. It is also able to export the scans and
 	return cmd
 }
 
-func initCmd(rootCmd *cobra.Command) {
-	rootCmd.PersistentFlags().StringP("log-level", "", "", "Log level (trace,info,fatal,panic,warn, debug) default is debug")
-	rootCmd.PersistentFlags().StringP("log-type", "", "", "Log type (text,json)")
-
-	rootCmd.PersistentFlags().StringP("tenable-access-key", "a", "", "tenable access key")
-	rootCmd.PersistentFlags().StringP("tenable-secret-key", "s", "", "tenable secret key")
-	rootCmd.PersistentFlags().StringP("tenable-scan-id", "i", "", "tenable scanID")
-
-
-	rootCmd.PersistentFlags().BoolP("include-gcloud", "g", false, "Include Google Cloud Instances In Report")
-	rootCmd.PersistentFlags().StringP("gcloud-service-account-path", "", "", "Path of service account token. Uses default if not specified")
-	rootCmd.PersistentFlags().StringP("gcloud-project", "p", "", "GCloud project to list instances from")
-
-	rootCmd.PersistentFlags().BoolP("include-aws", "A", false, "Include AWS Instances In Report")
-
-
-	rootCmd.PersistentFlags().BoolP("generate-report", "R", false, "Generate A report after the scan is complete")
-	rootCmd.PersistentFlags().BoolP("low-severity", "L", false, "Add Low Severity To Report")
-	rootCmd.PersistentFlags().BoolP("medium-severity", "M", false, "Add Medium Severity To Report")
-	rootCmd.PersistentFlags().BoolP("high-severity", "H", false, "Add High Severity To Report")
-	rootCmd.PersistentFlags().BoolP("critical-severity", "C", false, "Add Critical Severity To Report")
-
-	rootCmd.PersistentFlags().StringP("filter-search-type", "", "", "Search type to use in report. Only (and, or) are supported")
-	rootCmd.PersistentFlags().StringP("report-format", "", "", "Report Format of the scan. Support formats are Nessus, HTML, PDF, CSV, or DB")
-	rootCmd.PersistentFlags().StringP("report-chapters", "", "", "Chapters to include in the report")
-	rootCmd.PersistentFlags().BoolP("summary-report", "S", false, "Generate A report in summary format")
-	rootCmd.PersistentFlags().BoolP("full-report", "F", false, "Generate A report with all chapters")
-	rootCmd.PersistentFlags().StringP("report-file-location", "", "", "File Location of the report")
-}
-
-
 func setupLogging(logConfig *logConfig) error {
 
 	if logConfig.LogType == "json" {
