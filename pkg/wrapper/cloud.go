b130c3ac166cb1c205246b790c99565da07f53dd
maximillianbrain1 <mbrain@invoca.com>
2020-08-19 13:07:15 -0400

    Vendored code added to branch

--- pkg/wrapper/cloud.go
+++
@@ -1,9 +0,0 @@
-package wrapper
-
-import "github.com/Invoca/tenable-scan-launcher/pkg/config"
-
-type CloudWrapper interface {
-	Setup(config *config.BaseConfig) error
-	GatherIPs() ([]string, error)
-}
-
